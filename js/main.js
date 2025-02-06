eedd67b1dddae4b620d011b9835ba1afed9902b7
surriyoung <soowool486@naver.com>
2025-02-06 13:14:23 +0900

    250206

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const lenis = new Lenis({
+  duration: 2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+})
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
 $(document).ready(function () {
   let $scrollTopBtn = $(".scroll-top");
 
